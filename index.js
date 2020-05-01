09b271ed06e3f80f5712dddbcf0b7af6009fb030
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-01 16:26:15 -0400

    Trying again with the indentations /:

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 class Hello extends React.Component{
   render(){
     return (
-			<div>
+      <div>
         <h1>Hello World from React Component</h1>
         <p>Im a paragraph inside the component</p>
-				<h2>Hello there!</h2>
-			</div>
+      </div>
 		);
   }
 }
