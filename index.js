a5705202731c5627ef012319a6eb7a2780ec61f4
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-04 19:58:14 -0400

    re-write component to function component

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-class Hello extends React.Component{
-  render(){
-    return (
-      <div>
-        <h1>Hello World from React Component</h1>
-        <p>Im a paragraph inside the component</p>
-        <p>Im another paragraph inside the component</p>
-      </div>
-		);
-  }
-}
+const Hello = ()=>{
+  return (
+    <div>
+    <h1>Hello World from React Component</h1>
+    <p>Im a paragraph inside the component</p>
+    <p>Im another paragraph inside the component</p>
+  </div>
+  );
+};
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
 
