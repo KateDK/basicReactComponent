464fd8bcfcdc7b0d54a3b0a8b7651f12b0705e85
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-04 21:56:55 -0400

    Create Hello componentt with old component code

---
+++ Hello.js
@@ -0,0 +1,17 @@
+const todaysLuckyNumber = () =>{
+  return Math.floor(Math.random() * 10) +1;
+};
+
+const Hello = ()=>{
+  const num = todaysLuckyNumber();
+  return (
+    <div>
+      <h1>Hello World from React Component</h1>
+      <h2>Today's Lucky Number is: {num}</h2>
+      <p>{num === 7 ? "Wow, number seven extra lucky!" : "Looks like a good number, but I wouldn't bet on it!"}</p>
+  </div>
+  );
+};
+
+//ReactDOM.render(<Hello />, document.getElementById('root'));
+
