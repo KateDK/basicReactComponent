fc2f02650e4f8fa7b7c3b209aa6c0d0a5e5e6e9e
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-04 21:57:59 -0400

    Create App in index.js and render Hello component

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
-const todaysLuckyNumber = () =>{
-  return Math.floor(Math.random() * 10) +1;
-};
+class App extends React.Component{
+  render(){
+    return (
+      <div>
+        <Hello />
+      </div>
+    );
+  }
+}
 
-const Hello = ()=>{
-  const num = todaysLuckyNumber();
-  return (
-    <div>
-    <h1>Hello World from React Component</h1>
-    <h2>Today's Lucky Number is: {num}</h2>
-  <p>{num === 7 ? "Wow, number seven extra lucky!" : "Looks like a good number, but I wouldn't bet on it!"}</p>
-  </div>
-  );
-};
-
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
