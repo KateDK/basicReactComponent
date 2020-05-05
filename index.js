71cd0e62063213a2f67c0f01ca69f0ec19d09434
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-04 21:35:36 -0400

    add conditional login into component

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const todaysLuckyNumber = () =>{
 };
 
 const Hello = ()=>{
+  const num = todaysLuckyNumber();
   return (
     <div>
     <h1>Hello World from React Component</h1>
-    <h2>Today's Lucky Number is: {todaysLuckyNumber()}</h2>
+    <h2>Today's Lucky Number is: {num}</h2>
+  <p>{num === 7 ? "Wow, number seven extra lucky!" : "Looks like a good number, but I wouldn't bet on it!"}</p>
   </div>
   );
 };
