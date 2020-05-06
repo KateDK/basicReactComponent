3af5674d3ff2d4b9efe56d5878ead4521ce62072
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-06 16:32:21 -0400

    Added bangs

--- Hello.js
+++ Hello.js
@@ -2,10 +2,11 @@
 
 const Hello = (props)=>{
   const {name,num}= props;
+  const bangs = "!".repeat(num);
   return (
     <div>
       <h1>{name}'s Lucky Number for today is: {num}</h1>
-      <p>{num === 7 ? `Wow ${name}, number seven extra lucky!` : `Looks like a good number ${name}, but I wouldn't bet on it!`}</p>
+      <p>{num === 7 ? `Wow ${name}, number seven extra lucky${bangs}` : `Looks like a good number ${name}, but I wouldn't bet on it${bangs}`}</p>
   </div>
   );
 };
