1f1b99ea47d1437da1fce9d4cc043c36a6f22759
Kate Dubitski <kate.dubitski@gmail.com>
2020-05-06 16:22:11 -0400

    Pass name and num as props to Hello

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const todaysLuckyNumber = () =>{
   return Math.floor(Math.random() * 10) +1;
 };
+
 class App extends React.Component{
   render(){
     return (
       <div>
-        <Hello />
+        <Hello name="Daniel" num={todaysLuckyNumber()}/>
+        <Hello name="Kate" num={todaysLuckyNumber()}/>
+
       </div>
     );
   }
